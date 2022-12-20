7e630d4eb6630806ca7cf1e0acfdee320a3a0d22
Prashun Chitrakar <prashunchi@gmail.com>
2022-12-20 20:11:24 +0545

    remove gutter left gutter

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,24 +1,28 @@
 import { useCallback, useContext } from "react";
 import CodeMirror from "@uiw/react-codemirror";
-import { vscodeDark } from "@uiw/codemirror-theme-vscode";
 import { markdown, markdownLanguage } from "@codemirror/lang-markdown";
 import { languages } from "@codemirror/language-data";
 import { EditorContext } from "../contexts/EditorContext";
 import { ViewUpdate } from "@codemirror/view";
+import { githubDark } from "@uiw/codemirror-theme-github";
 
 const Editor = () => {
   const { value, onChange } = useContext(EditorContext);
 
   const updateValue = useCallback((value: string, _viewUpdate: ViewUpdate) => {
-    console.log(value);
     onChange(value);
   }, []);
 
   return (
     <CodeMirror
-      theme={vscodeDark}
+      theme={githubDark}
       value={value}
-      height="200px"
+      height="100%"
+      className="editor"
+      basicSetup={{
+        foldGutter: false,
+        lineNumbers: false,
+      }}
       extensions={[
         markdown({ base: markdownLanguage, codeLanguages: languages }),
       ]}
